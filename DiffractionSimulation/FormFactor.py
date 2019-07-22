0c3fe6510469939a2bdd98627bce42090e6de3dd
bshev <bshevits@gmail.com>
2019-07-22 13:54:02 -0700

    added references and ForFactor

---
+++ DiffractionSimulation/FormFactor.py
@@ -0,0 +1,16 @@
+import numpy as np
+import os
+
+aff_dict = np.load(os.path.join('Atomic_Form_Factors','Atomic_Form_Factor_Dictionary.npy')).item()
+    
+def FormFactor(q,elem): 
+    ''' 
+    Loads atomic form factor function from tabulated data. 
+    Element can be symbol or atomic number e.g. 'C' or 6. 
+    Lengths (or inverse lengths) are in Angstroms
+    '''
+    a = aff_dict[elem][1]
+    coeff = a[1:]
+    coeff = np.array([coeff[0:5],coeff[5:10]])
+    val = coeff[0,0]*np.exp(-coeff[1,0]*((q/(2*np.pi))**2)) + coeff[0,1]*np.exp(-coeff[1,1]*((q/(2*np.pi))**2)) + coeff[0,2]*np.exp(-coeff[1,2]*((q/(2*np.pi))**2)) + coeff[0,3]*np.exp(-coeff[1,3]*((q/(2*np.pi))**2)) + coeff[0,4]*np.exp(-coeff[1,4]*((q/(2*np.pi))**2))
+    return val
